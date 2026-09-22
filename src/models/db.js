8b5c5d7210cb8e35940f99203e2160431899c883
murungi197 <murungiinnocent197@gmail.com>
2026-09-22 23:46:38 +0100

    latest fix

--- src/models/db.js
+++ src/models/db.js
@@ -1,14 +1,10 @@
 import "dotenv/config";
 import { Pool } from "pg";
 
-
 const pool = new Pool({
   connectionString: process.env.DB_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
+  ssl: process.env.DB_SSL === "true" ? { rejectUnauthorized: false } : false,
 });
-
 
 let db = null;
 
